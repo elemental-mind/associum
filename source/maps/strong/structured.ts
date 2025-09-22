b90708095876deca8e5de8be9bd4d83e1b547b9d
Magnus Meseck <magnus.meseck@googlemail.com>
2025-09-22 20:31:26 +0200

    feat: Draft implementation for strong structured maps

--- source/maps/strong/structured.ts
+++ source/maps/strong/structured.ts
@@ -1,29 +1,162 @@
+import type { MultikeyMapQueryResult } from "../../associatium.ts";
 import { MultikeyMap } from './base';
 
-export class StructuredMultiKeyMap<K extends object, V> extends MultikeyMap<K,V>
+export class StructuredMultiKeyMap<K extends Record<string, any>, V> extends MultikeyMap<K, V>
 {
-    set(key: Readonly<K>, value: V): void
+    protected fieldCount = 0;
+    protected fieldMap: Record<string, number> = Object.create(null);
+
+    constructor()
     {
-        throw new Error("Not implemented");
+        super();
     }
 
-    get(key: Readonly<K>): V | undefined
+    encodeSettingComposite(keys: K): string
     {
-        throw new Error("Not implemented");
+        const keyletArray: any[] = [];
+
+        for (const key in keys)
+        {
+            const arrayPosition = this.fieldMap[key] ?? this.registerNewField(key);
+            keyletArray[arrayPosition] = this.ensureAndReturnKeylet(keys[key]);
+        }
+
+        return this.keyletsToComposite(keyletArray);
     }
 
-    has(key: Readonly<K>): boolean
+    encodeProbingComposite(keys: K): string | undefined
     {
-        throw new Error("Not implemented");
+        const probeArray = this.encodeProbingArray(keys);
+        return probeArray ? this.keyletsToComposite(probeArray) : undefined;
     }
 
-    delete(key: Readonly<K>): boolean
+    protected encodeProbingArray(keys: Partial<K>)
     {
-        throw new Error("Not implemented");
+        const keyletArray: string[] = [];
+
+        for (const key in keys)
+        {
+            const arrayPosition = this.fieldMap[key];
+            if (arrayPosition === undefined)
+                return undefined;
+
+            const keylet = MultikeyMap.objectsToKeylets.get(keys[key]);
+            if (keylet === undefined)
+                return undefined;
+
+            keyletArray[arrayPosition] = keylet;
+        }
+
+        return keyletArray;
     }
 
-    query(partial: Readonly<Partial<K>>): Array<{ key: K, value: V; }>
+    private registerNewField(name: string): number
     {
-        throw new Error("Not implemented");
+        this.fieldMap[name] = this.fieldCount++;
+        return this.fieldMap[name];
+    }
+
+
+}
+
+export class QueryableStructuredMultiKeyMap<K extends Record<string, any>, V> extends StructuredMultiKeyMap<K, V>
+{
+    protected keyletToComposites = new Map<string, string>();
+
+    set(keys: K, value: V): void
+    {
+        const composite = this.encodeSettingComposite(keys);
+
+        if (!this.map.has(composite))
+            return;
+
+        for (const field in keys)
+        {
+            const keylet = MultikeyMap.objectsToKeylets.get(keys[field])!;
+
+            this.linkKeyletToComposite(keylet, composite);
+        }
+
+        this.map.set(composite, value);
+    }
+
+    query(queryTemplate: Partial<K>): MultikeyMapQueryResult<K, V>[]
+    {
+        const requiredKeyletsInOrder = this.encodeProbingArray(queryTemplate);
+        if (!requiredKeyletsInOrder) return [];
+
+        const compositeIndicesThatNeedToMatchQueryKeylets: number[] = [];
+        for (let index = 0; index < requiredKeyletsInOrder.length; index++)
+            if (requiredKeyletsInOrder[index] !== undefined) compositeIndicesThatNeedToMatchQueryKeylets.push(index);
+
+        const alreadyCheckedComposites = new Set<string>();
+        const result: MultikeyMapQueryResult<K, V>[] = [];
+
+        keyletLoop:
+        for (const keyletIndex of compositeIndicesThatNeedToMatchQueryKeylets)
+        {
+            const keylet = requiredKeyletsInOrder[keyletIndex];
+            const compositesContainingKeylet = this.keyletToComposites.get(keylet)!;
+
+            compositeLoop:
+            for (const composite of compositesContainingKeylet.split("|"))
+            {
+                if (alreadyCheckedComposites.has(composite)) continue;
+
+                alreadyCheckedComposites.add(composite);
+
+                const compositeKeylets = composite.split("_");
+
+                compositeCheckLoop:
+                for (const index of compositeIndicesThatNeedToMatchQueryKeylets)
+                    if (compositeKeylets[index] !== requiredKeyletsInOrder[index]) continue compositeLoop;
+
+                result.push(
+                    {
+                        key: this.buildKeyObject(compositeKeylets) as K,
+                        value: this.map.get(composite)!
+                    });
+            }
+        }
+
+        return result;
+    }
+
+    delete(keys: K): boolean
+    {
+        const composite = this.encodeProbingComposite(keys);
+        if (!composite || !this.map.delete(composite))
+            return false;
+
+        //TODO
+
+        this.freeComposite(composite);
+
+        return true;
+    }
+
+    clear()
+    {
+        super.clear();
+        this.keyletToComposites.clear();
+    }
+
+    private linkKeyletToComposite(keylet: string, composite: string): void
+    {
+        const existing = this.keyletToComposites.get(keylet);
+        this.keyletToComposites.set(keylet, existing ? `${existing}|${composite}` : composite);
+    }
+
+    private buildKeyObject(keyKeylets: string[])
+    {
+        const keyObject = {} as Record<string, any>;
+        for (const key in this.fieldMap)
+        {
+            const keyIndex = this.fieldMap[key];
+
+            if (keyKeylets[keyIndex])
+                keyObject[key] = MultikeyMap.keyletsToObjects.get(keyKeylets[keyIndex]);
+        }
+        return keyObject;
     }
 }
